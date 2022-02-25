51eeb026fdcc2a8e077ec87c1881a7a15142cc66
Ruilu Gao <rgao23@cmc.edu>
2022-02-24 22:10:34 -0800

    QUESTIONS SOLVED

--- sql/02.sql
+++ sql/02.sql
@@ -1,3 +1,12 @@
 /*
  * Compute the country with the most customers in it. 
  */
+
+SELECT country
+FROM country
+JOIN city using (country_id)
+JOIN address using (city_id)
+JOIN customer using (address_id)
+GROUP BY country
+ORDER BY count(customer_id) DESC
+LIMIT 1;
--- sql/05.sql
+++ sql/05.sql
@@ -6,3 +6,13 @@
  * HINT:
  * This can be solved with a self join on the film_actor table.
  */
+
+SELECT title FROM film
+JOIN film_actor USING (film_id)
+JOIN actor USING (actor_id)
+WHERE actor_id IN (SELECT actor_id from actor
+JOIN film_actor USING (actor_id)
+WHERE film_id=21)
+GROUP BY title,film_id
+HAVING count(film_id)>=2
+ORDER BY title;
